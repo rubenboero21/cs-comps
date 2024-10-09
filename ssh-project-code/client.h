be6ed8ab11cbc33623dee9dea7d760fdfbd2419f
Ruben Boero <boeror2017@gmail.com>
2024-10-09 09:13:31 -0500

    Changed formatting of RawByteArray struct and added comments

--- ssh-project-code/client.h
+++ ssh-project-code/client.h
@@ -10,14 +10,14 @@
 typedef struct {
     unsigned char *data;
     size_t size;
-} rawByteArray;
+} RawByteArray;
 
 // Return a packet in Binary Packet Protocol with the given payload
 /*
 Input: Pointer to the payload of a binary packet protocol packet
 Output: A buffer containing the raw bytes of the packet.
 */
-BinaryPacket constructPacket(unsigned char *payload);
+// BinaryPacket constructPacket(unsigned char *payload);
 
 /*
 Input: None (potentially a list of algorithms in the future)
@@ -26,17 +26,24 @@ Output: The payload in string form (the list of comma separated algorithms with 
 unsigned char *constructKexPayload();
 
 /*
+Input: The socket to send the protocol packet to
+Output:
 */
 int sendProtocol(int sock);
 
 /*
+Input: The number of random bytes to generate
+Output: A pointer to a RawByteArray struct that contains the random bytes in the data variable
+        and the size of the data in size variable
 */
-unsigned char* generateRandomBytes(int numBytes);
+RawByteArray* generateRandomBytes(int numBytes);
 
 /*
 */
 int sendKexInit (int sock);
 
 /*
+Input: A host and port number to connect to 
+Output: SOME ERROR CODES - NEED TO UPDATE
 */
 int start_client(const char *host, const int port);
